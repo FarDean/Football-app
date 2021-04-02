ca904f0eb2c3791addbd91d584866c33bb375674
FarDean <sfh1212@gmail.com>
2021-04-02 12:06:19 +0430

    added redux store and mirage js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
-import "./App.css";
 import { useState, useEffect } from "react";
-import { config } from "./config";
 
 function App() {
 	const [shit, setShit] = useState(null);
 
 	useEffect(() => {
 		async function getShit() {
-			const res = await fetch("https://v3.football.api-sports.io/status", {
-				headers: {
-					"x-rapidapi-host": config.host,
-					"x-rapidapi-key": config.key,
-				},
-			});
+			const res = await fetch("/api/users");
 			const data = await res.json();
 			setShit(data);
 		}
--- src/api/client.js
+++
@@ -1,4 +0,0 @@
-import { createServer } from "miragejs";
-
-let server = createServer();
-server.get("/api/users", { users: [{ id: 1, name: "Bob" }] });
