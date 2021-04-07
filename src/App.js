ed73de75830960cb19bdc9bc6be8cff843bbaa63
FarDean <sfh1212@gmail.com>
2021-04-07 13:19:39 +0430

    added live score comp

--- src/App.js
+++ src/App.js
@@ -3,26 +3,27 @@ import { config } from "./config";
 import { Link } from "react-router-dom";
 import { Menu, Segment, Container } from "semantic-ui-react";
 
+import { LiveScores } from "./comps/LiveScores";
 function App() {
-	const [teams, setTeams] = useState({});
-	useEffect(() => {
-		async function getTeams() {
-			const res = await fetch(
-				"https://v3.football.api-sports.io/fixtures?live=39-135-140-78",
-				{
-					method: "GET",
-					headers: {
-						"x-rapidapi-host": config.host,
-						"x-rapidapi-key": config.key,
-					},
-				}
-			);
-			const data = await res.json();
-			setTeams(data.response);
-		}
-		getTeams();
-	}, []);
-	console.log(teams);
+	// const [teams, setTeams] = useState({});
+	// useEffect(() => {
+	// 	async function getTeams() {
+	// 		const res = await fetch(
+	// 			"https://v3.football.api-sports.io/fixtures?live=39-135-140-78",
+	// 			{
+	// 				method: "GET",
+	// 				headers: {
+	// 					"x-rapidapi-host": config.host,
+	// 					"x-rapidapi-key": config.key,
+	// 				},
+	// 			}
+	// 		);
+	// 		const data = await res.json();
+	// 		setTeams(data.response);
+	// 	}
+	// 	getTeams();
+	// }, []);
+	// console.log(teams);
 
 	const [activeItem, setActiveItem] = useState("home");
 
@@ -50,7 +51,7 @@ function App() {
 					/>
 				</Menu.Menu>
 			</Menu>
-			<Link to="/leagues">leagues</Link>
+			<LiveScores />
 		</Container>
 	);
 }
--- src/comps/LiveScores.jsx
+++ src/comps/LiveScores.jsx
@@ -1,3 +1,19 @@
+import { useSelector, useDispatch } from "react-redux";
+import { fetchLiveScores } from "./../redux/liveScoreSlice";
+import { useEffect } from "react";
+
 export const LiveScores = () => {
+	const dispatch = useDispatch();
+	const liveScores = useSelector(state => state.livescore.liveScores);
+	const liveScoreStatus = useSelector(state => state.livescore.status);
+
+	useEffect(() => {
+		if (liveScoreStatus === "idle") {
+			dispatch(fetchLiveScores());
+		}
+	}, [dispatch, liveScoreStatus]);
+
+	console.log(liveScores);
+
 	return <h1>LiveScores</h1>;
 };
