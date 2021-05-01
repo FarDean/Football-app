72d9b7dc5fb2ea490ecd1c8be2881855afdeb386
FarDean <sfh1212@gmail.com>
2021-05-01 10:05:59 +0430

    added single fixture slice

--- src/redux/fixtureSlice.js
+++
@@ -1,6 +0,0 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-
-// Thunk
-const fetchSingleFixture = createAsyncThunk("singleFixture/fetchSingleFixture", async () => {
-	const res = await fetch();
-});
