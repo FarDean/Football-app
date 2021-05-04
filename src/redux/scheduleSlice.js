e588a5c34eafe99fac40705ea6b3737d01651d0f
FarDean <sfh1212@gmail.com>
2021-05-04 12:09:26 +0430

    ss

--- src/redux/scheduleSlice.js
+++ src/redux/scheduleSlice.js
@@ -4,7 +4,24 @@ import { config } from "./../config";
 // Thunk
 export const fetchSchedule = createAsyncThunk(
 	"schedule/fetchSchedule",
-	async ({ lastWeek, nextWeek }) => {
-		const res = await fetch;
+	async ({ leagueId,lastWeek, nextWeek }) => {
+		const res = await fetch(`https://v3.football.api-sports.io/fixtures?league=${leagueId}&season=${config.defaultSeason}&from=${lastWeek}&to=${nextWeek}`,{
+            method:'GET',
+            headers:{
+                "x-rapidapi-host": config.host,
+				"x-rapidapi-key": config.key,
+            }
+        })
+        const data = await res.json()
+        return data.response
 	}
 );
+
+// Slice
+export const scheduleSlice = createSlice({
+    name:'schedule',
+    initialState:{
+        status:'idle',
+        schedule:
+    }
+})
