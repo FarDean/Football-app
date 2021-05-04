6067cc3ddf143345c44983cc5c7889032b06c780
FarDean <sfh1212@gmail.com>
2021-05-04 12:17:51 +0430

    create schedule slice and added them to store

--- src/redux/scheduleSlice.js
+++ src/redux/scheduleSlice.js
@@ -4,24 +4,44 @@ import { config } from "./../config";
 // Thunk
 export const fetchSchedule = createAsyncThunk(
 	"schedule/fetchSchedule",
-	async ({ leagueId,lastWeek, nextWeek }) => {
-		const res = await fetch(`https://v3.football.api-sports.io/fixtures?league=${leagueId}&season=${config.defaultSeason}&from=${lastWeek}&to=${nextWeek}`,{
-            method:'GET',
-            headers:{
-                "x-rapidapi-host": config.host,
-				"x-rapidapi-key": config.key,
-            }
-        })
-        const data = await res.json()
-        return data.response
+	async ({ leagueId, lastWeek, nextWeek }) => {
+		const res = await fetch(
+			`https://v3.football.api-sports.io/fixtures?league=${leagueId}&season=${config.defaultSeason}&from=${lastWeek}&to=${nextWeek}`,
+			{
+				method: "GET",
+				headers: {
+					"x-rapidapi-host": config.host,
+					"x-rapidapi-key": config.key,
+				},
+			}
+		);
+		const data = await res.json();
+		return data.response;
 	}
 );
 
 // Slice
 export const scheduleSlice = createSlice({
-    name:'schedule',
-    initialState:{
-        status:'idle',
-        schedule:
-    }
-})
+	name: "schedule",
+	initialState: {
+		status: "idle",
+		schedule: [],
+		error: null,
+	},
+	reducers: {},
+	extraReducers: {
+		[fetchSchedule.pending]: (state, action) => {
+			state.status = "loading";
+		},
+		[fetchSchedule.fulfilled]: (state, action) => {
+			state.status = "succeeded";
+			state.schedule = action.payload;
+		},
+		[fetchSchedule.rejected]: (state, action) => {
+			state.status = "failed";
+			state.error = action.error.message;
+		},
+	},
+});
+
+export default scheduleSlice.reducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -4,6 +4,8 @@ import teamReducer from "./teamSlice";
 import statReducer from "./statsSlice";
 import liveScoreReducer from "./liveScoreSlice";
 import standingReducer from "./standingSlice";
+import scheduleReducer from "./scheduleSlice";
+import singleFixtureReducer from "./singleFixtureSlice";
 
 export default configureStore({
 	reducer: {
@@ -12,5 +14,7 @@ export default configureStore({
 		stat: statReducer,
 		livescore: liveScoreReducer,
 		standing: standingReducer,
+		schedule: scheduleReducer,
+		singleFixture: singleFixtureReducer,
 	},
 });
