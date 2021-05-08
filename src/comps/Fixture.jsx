8258c230a842aa98f2d8dd03c7a6ebaea553066a
FarDean <sfh1212@gmail.com>
2021-05-08 10:02:58 +0430

    work in progress on fixture comp

--- src/comps/Fixture.jsx
+++ src/comps/Fixture.jsx
@@ -1,7 +1,35 @@
-export const Fixture = () => {
-	return (
-		<>
-			<h1>single fixture</h1>
-		</>
-	);
-};
+import { fetchSingleFixture } from "./../redux/singleFixtureSlice";
+import { useSelector, useDispatch } from "react-redux";
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Loader } from "./utils/Loader";
+import { Error } from "./utils/Error";
+
+export const Fixture = React.memo(() => {
+	const dispatch = useDispatch();
+	const fixture = useSelector(state => state.singleFixture.fixture);
+	const fixtureStatus = useSelector(state => state.singleFixture.status);
+	const error = useSelector(state => state.singleFixture.error);
+
+	let { fixtureId } = useParams();
+
+	// useEffect(() => {
+	// 	if (fixtureStatus === "idle") {
+	// 		dispatch(fetchSingleFixture(fixtureId));
+	// 	}
+	// }, [dispatch, fixtureId, fixtureStatus]);
+
+	console.log(fixture);
+
+	if (fixtureStatus === "loading") return <Loader />;
+
+	if (fixtureStatus === "failed") return <Error text={error} />;
+	if (fixtureStatus === "succeeded")
+		return (
+			<>
+				<div>
+					<span>&#8249;</span> Back
+				</div>
+			</>
+		);
+});
