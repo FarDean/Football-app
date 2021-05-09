6d1f6fef78ff0da6b890993efd1084d0461ccad3
FarDean <sfh1212@gmail.com>
2021-05-09 11:56:47 +0430

    new styles for loader and error comps

--- src/comps/utils/Error.jsx
+++ src/comps/utils/Error.jsx
@@ -1,4 +1,4 @@
-import styles from "./../../styles/Utils.module.css";
+import styles from "./../../styles/utils/Error.module.css";
 export const Error = ({ text = "Something went Wrong, Try Refreshing!" }) => {
 	return (
 		<div className={styles.error}>
--- src/comps/utils/Loader.jsx
+++ src/comps/utils/Loader.jsx
@@ -1,7 +1,14 @@
+import styles from "./../../styles/utils/Loader.module.css";
+
 export const Loader = () => {
 	return (
-		<div className="flex">
-			<div className="loader"></div>
+		<div className={styles.container}>
+			<div className={styles.box}>
+				<div className={styles.shadow}></div>
+				<div className={styles.gravity}>
+					<div className={styles.ball}></div>
+				</div>
+			</div>
 		</div>
 	);
 };
