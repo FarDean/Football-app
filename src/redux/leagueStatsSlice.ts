9e8a16c85bef26b4a37202ca90a164bf4d272b88
FarDean <sfh1212@gmail.com>
2021-05-26 11:06:28 +0430

    working on Promise.all

--- src/redux/leagueStatsSlice.ts
+++ src/redux/leagueStatsSlice.ts
@@ -1,2 +1,23 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { config } from "./../config";
+
+// Thunk
+export const fetchLeagueStats = createAsyncThunk(
+	"leagueStats/fetchLeagueStats",
+	async (leagueId: string | number) => {
+		const getTopScorers = fetch(
+			`https://v3.football.api-sports.io/players/topscorers?season=${config.defaultSeason}&league=${leagueId}`
+		);
+		const getTopAssists = fetch(
+			`https://v3.football.api-sports.io/players/topassists?season=${config.defaultSeason}&league=${leagueId}`
+		);
+		const getTopYellowCards = fetch(
+			`https://v3.football.api-sports.io/players/topyellowcards?season=${config.defaultSeason}&league=${leagueId}`
+		);
+		const getTopRedCards = fetch(
+			`https://v3.football.api-sports.io/players/topredcards?season=${config.defaultSeason}&league=${leagueId}`
+		);
+
+		const res = await Promise.all();
+	}
+);
