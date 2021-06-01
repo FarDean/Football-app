0ead2b41ff9ecf65fc9b96bcd716c450d9938ecf
FarDean <sfh1212@gmail.com>
2021-06-01 22:31:23 +0430

    implementd Link to TeamDetail comp in Standing comp

--- src/comps/Standing.tsx
+++ src/comps/Standing.tsx
@@ -4,6 +4,8 @@ import React, { useEffect } from "react";
 import { Loader } from "./utils/Loader";
 import { Error } from "./utils/Error";
 import styles from "./../styles/Standing.module.css";
+import slugify from "slugify";
+import { Link } from "react-router-dom";
 
 const icons: { [key: string]: any } = {
 	W: <i className={`fas fa-check-circle ${styles.win}`}></i>,
@@ -45,28 +47,34 @@ export const Statnding: React.FC<Props> = ({ leagueId }) => {
 					</tr>
 
 					{standing[0].league.standings[0].map((team: any, i: number) => (
-						<tr className={styles.tbrow} key={i}>
-							<td className={styles.flex}>
-								<span className={styles.span}>{team.rank}</span>
-								<img className={styles.img} src={team.team.logo} alt="" />{" "}
-								<span>{team.team.name}</span>
-							</td>
-							<td>{team.all.played}</td>
-							<td>{team.all.win}</td>
-							<td>{team.all.draw}</td>
-							<td>{team.all.lose}</td>
-							<td>{team.all.goals.for}</td>
-							<td>{team.all.goals.against}</td>
-							<td>{team.goalsDiff}</td>
-							<td>{team.points}</td>
-							<td className={styles.last5}>
-								{[...team.form].map((str, i) => (
-									<span key={i} className={styles.form}>
-										{icons[str]}
-									</span>
-								))}
-							</td>
-						</tr>
+						<>
+							<Link
+								to={`/team/${slugify(team.team.name)}`}
+								className={styles.tbrow}
+								key={i}
+							>
+								<td className={styles.flex}>
+									<span className={styles.span}>{team.rank}</span>
+									<img className={styles.img} src={team.team.logo} alt="" />{" "}
+									<span>{team.team.name}</span>
+								</td>
+								<td>{team.all.played}</td>
+								<td>{team.all.win}</td>
+								<td>{team.all.draw}</td>
+								<td>{team.all.lose}</td>
+								<td>{team.all.goals.for}</td>
+								<td>{team.all.goals.against}</td>
+								<td>{team.goalsDiff}</td>
+								<td>{team.points}</td>
+								<td className={styles.last5}>
+									{[...team.form].map((str, i) => (
+										<span key={i} className={styles.form}>
+											{icons[str]}
+										</span>
+									))}
+								</td>
+							</Link>
+						</>
 					))}
 				</tbody>
 			</table>
