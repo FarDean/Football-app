967815460d23ee3758dfb14a56ccaca6390138a5
FarDean <sfh1212@gmail.com>
2021-06-01 23:10:25 +0430

    unsuccesful deploy because of unused vars

--- src/comps/TeamDetail.tsx
+++ src/comps/TeamDetail.tsx
@@ -1,6 +1,3 @@
-import { useAppDispatch, useAppSelector } from "./../redux/hooks";
-import { useEffect } from "react";
-
 export const TeamDetail: React.FC = (): JSX.Element => {
 	return <h1>Team Detail</h1>;
 };
