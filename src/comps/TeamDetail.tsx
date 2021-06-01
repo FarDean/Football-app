162a84ce6b8cd9ab1ae93e1be2b4a02a074531ca
FarDean <sfh1212@gmail.com>
2021-06-01 22:15:05 +0430

    created TEamDetail Comp

---
+++ src/comps/TeamDetail.tsx
@@ -0,0 +1,6 @@
+import { useAppDispatch, useAppSelector } from "./../redux/hooks";
+import { useEffect } from "react";
+
+export const TeamDetail: React.FC = (): JSX.Element => {
+	return <h1>Team Detail</h1>;
+};
