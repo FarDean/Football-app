a4a799d6bfcf258dd3130e3718e3e70759d4c139
FarDean <64462409+FarDean@users.noreply.github.com>
2021-06-06 21:36:34 +0430

    prettier new setting

--- src/comps/Fixture.tsx
+++ src/comps/Fixture.tsx
@@ -1,6 +1,6 @@
 import { fetchSingleFixture } from "../redux/singleFixtureSlice";
 import { useAppSelector, useAppDispatch } from "./../redux/hooks";
-import React, { useEffect } from "react";
+import { memo, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { Loader } from "./utils/Loader";
 import { Error } from "./utils/Error";
@@ -10,153 +10,155 @@ import { formatRelative } from "date-fns";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
 
-export const Fixture: React.FC = React.memo((): JSX.Element => {
-	const dispatch = useAppDispatch();
-	const fixture = useAppSelector(state => state.singleFixture.fixture);
-	const fixtureStatus = useAppSelector(state => state.singleFixture.status);
-	const error = useAppSelector(state => state.singleFixture.error);
+export const Fixture: React.FC = memo(
+	(): JSX.Element => {
+		const dispatch = useAppDispatch();
+		const fixture = useAppSelector(state => state.singleFixture.fixture);
+		const fixtureStatus = useAppSelector(state => state.singleFixture.status);
+		const error = useAppSelector(state => state.singleFixture.error);
 
-	let { fixtureId }: { fixtureId: string } = useParams();
+		let { fixtureId }: { fixtureId: string } = useParams();
 
-	useEffect(() => {
-		dispatch(fetchSingleFixture(fixtureId));
-	}, [dispatch, fixtureId]);
+		useEffect(() => {
+			dispatch(fetchSingleFixture(fixtureId));
+		}, [dispatch, fixtureId]);
 
-	console.log(fixture);
+		console.log(fixture);
 
-	if (fixtureStatus === "loading")
-		return (
-			<>
-				<Back />
-				<Loader />
-			</>
-		);
-	if (fixtureStatus === "failed")
-		return (
-			<>
-				<Back />
-				<Error text={error} />
-			</>
-		);
-	if (fixtureStatus === "succeeded")
-		return (
-			<>
-				<Back />
-				<div className={style.box}>
-					<div className={style.top}>
-						<div>{fixture.league.name}</div>
-						<div>Match Day: {fixture.league.round.slice(17)}</div>
-						<div>{formatRelative(new Date(fixture.fixture.date), new Date())}</div>
-						{fixture.fixture.status.long === "Match Finished" ||
-						fixture.fixture.status.long === "Not Started" ? (
-							<div>{fixture.fixture.status.long}</div>
-						) : fixture.fixture.status.long === "Halftime" ? (
-							<div>
-								{fixture.fixture.status.long}
-								<div className={style.container}>
-									<div className={style.loader}>
-										<span></span>
-										<span></span>
-										<span></span>
-										<span></span>
-									</div>
-								</div>
-							</div>
-						) : (
-							<div>
-								{fixture.fixture.status.elapsed}
-								<span>`</span>
-								<div className={style.container}>
-									<div className={style.loader}>
-										<span></span>
-										<span></span>
-										<span></span>
-										<span></span>
-									</div>
-								</div>
-							</div>
-						)}
-					</div>
-					<div className={style.teams}>
-						<div className={style.team}>
-							<div>
-								<img src={fixture.teams.home.logo} alt="" />
-							</div>
-							<div>{fixture.teams.home.name}</div>
-						</div>
-						<div className={style.score}>
-							<div>{fixture.goals.home}</div>
-							<div>-</div>
-							<div>{fixture.goals.away}</div>
-						</div>
-						<div className={style.team}>
-							<div>
-								<img src={fixture.teams.away.logo} alt="" />
-							</div>
-							<div>{fixture.teams.away.name}</div>
-						</div>
-					</div>
-					<div className={style.tabs}>
-						<Tabs>
-							<TabList className={style.tablist}>
-								<Tab>Statistics</Tab>
-								<Tab>Events</Tab>
-								<Tab>Line Ups</Tab>
-							</TabList>
-
-							<TabPanel>
-								{fixture.statistics.length === 0 ? (
-									<h2 className={style.h2}>No Statistics!</h2>
-								) : (
-									<div className={style.grid}>
-										<div className={style.div1}>
-											{fixture.statistics[0].statistics.map(
-												(stat: any, i: number) => (
-													<div key={i}>{stat.type}</div>
-												)
-											)}
-										</div>
-										<div className={style.div2}>
-											{fixture.statistics[0].statistics.map(
-												(stat: any, i: number) => (
-													<div key={i}>{stat.value ?? "0"}</div>
-												)
-											)}
-										</div>
-										<div className={style.div3}>
-											{fixture.statistics[1].statistics.map(
-												(stat: any, i: number) => (
-													<div key={i}>{stat.value ?? "0"}</div>
-												)
-											)}
-										</div>
-									</div>
-								)}
-							</TabPanel>
-							<TabPanel>
+		if (fixtureStatus === "loading")
+			return (
+				<>
+					<Back />
+					<Loader />
+				</>
+			);
+		if (fixtureStatus === "failed")
+			return (
+				<>
+					<Back />
+					<Error text={error} />
+				</>
+			);
+		if (fixtureStatus === "succeeded")
+			return (
+				<>
+					<Back />
+					<div className={style.box}>
+						<div className={style.top}>
+							<div>{fixture.league.name}</div>
+							<div>Match Day: {fixture.league.round.slice(17)}</div>
+							<div>{formatRelative(new Date(fixture.fixture.date), new Date())}</div>
+							{fixture.fixture.status.long === "Match Finished" ||
+							fixture.fixture.status.long === "Not Started" ? (
+								<div>{fixture.fixture.status.long}</div>
+							) : fixture.fixture.status.long === "Halftime" ? (
 								<div>
-									{fixture.events.map((event: any, i: number) => (
-										<div key={i}>
-											<div>
-												<div>{event.detail}</div>
-												<div>{event.team.name}</div>
-												<div>{event.time.elapsed}`</div>
+									{fixture.fixture.status.long}
+									<div className={style.container}>
+										<div className={style.loader}>
+											<span></span>
+											<span></span>
+											<span></span>
+											<span></span>
+										</div>
+									</div>
+								</div>
+							) : (
+								<div>
+									{fixture.fixture.status.elapsed}
+									<span>`</span>
+									<div className={style.container}>
+										<div className={style.loader}>
+											<span></span>
+											<span></span>
+											<span></span>
+											<span></span>
+										</div>
+									</div>
+								</div>
+							)}
+						</div>
+						<div className={style.teams}>
+							<div className={style.team}>
+								<div>
+									<img src={fixture.teams.home.logo} alt="" />
+								</div>
+								<div>{fixture.teams.home.name}</div>
+							</div>
+							<div className={style.score}>
+								<div>{fixture.goals.home}</div>
+								<div>-</div>
+								<div>{fixture.goals.away}</div>
+							</div>
+							<div className={style.team}>
+								<div>
+									<img src={fixture.teams.away.logo} alt="" />
+								</div>
+								<div>{fixture.teams.away.name}</div>
+							</div>
+						</div>
+						<div className={style.tabs}>
+							<Tabs>
+								<TabList className={style.tablist}>
+									<Tab>Statistics</Tab>
+									<Tab>Events</Tab>
+									<Tab>Line Ups</Tab>
+								</TabList>
+
+								<TabPanel>
+									{fixture.statistics.length === 0 ? (
+										<h2 className={style.h2}>No Statistics!</h2>
+									) : (
+										<div className={style.grid}>
+											<div className={style.div1}>
+												{fixture.statistics[0].statistics.map(
+													(stat: any, i: number) => (
+														<div key={i}>{stat.type}</div>
+													)
+												)}
 											</div>
-											<div>
-												<div>{event.player.name}</div>
-												<div>{event.assist.name ?? ""}</div>
+											<div className={style.div2}>
+												{fixture.statistics[0].statistics.map(
+													(stat: any, i: number) => (
+														<div key={i}>{stat.value ?? "0"}</div>
+													)
+												)}
+											</div>
+											<div className={style.div3}>
+												{fixture.statistics[1].statistics.map(
+													(stat: any, i: number) => (
+														<div key={i}>{stat.value ?? "0"}</div>
+													)
+												)}
 											</div>
 										</div>
-									))}
-								</div>
-							</TabPanel>
-							<TabPanel>
-								<h2>Any content 3</h2>
-							</TabPanel>
-						</Tabs>
+									)}
+								</TabPanel>
+								<TabPanel>
+									<div>
+										{fixture.events.map((event: any, i: number) => (
+											<div key={i}>
+												<div>
+													<div>{event.detail}</div>
+													<div>{event.team.name}</div>
+													<div>{event.time.elapsed}`</div>
+												</div>
+												<div>
+													<div>{event.player.name}</div>
+													<div>{event.assist.name ?? ""}</div>
+												</div>
+											</div>
+										))}
+									</div>
+								</TabPanel>
+								<TabPanel>
+									<h2>Any content 3</h2>
+								</TabPanel>
+							</Tabs>
+						</div>
 					</div>
-				</div>
-			</>
-		);
-	return <Loader />;
-});
+				</>
+			);
+		return <Loader />;
+	}
+);
--- src/comps/Wrapper.tsx
+++ src/comps/Wrapper.tsx
@@ -5,95 +5,101 @@ import { useAppSelector, useAppDispatch } from "./../redux/hooks";
 import styles from "./../styles/Wrapper.module.css";
 import { Error } from "./utils/Error";
 import { Loader } from "./utils/Loader";
-import React from "react";
+import { memo } from "react";
 import slugify from "slugify";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCaretDown, faCaretUp } from "@fortawesome/free-solid-svg-icons";
 import { Container } from "./utils/Container";
 import logo from "./../assets/logo.png";
 
-export const Wrapper: React.FC = React.memo((): JSX.Element => {
-	const dispatch = useAppDispatch();
+export const Wrapper: React.FC = memo(
+	(): JSX.Element => {
+		const dispatch = useAppDispatch();
 
-	const leagues = useAppSelector(state => state.league.leagues);
-	const leaguesStatus = useAppSelector(state => state.league.status);
-	const error = useAppSelector(state => state.league.error);
+		const leagues = useAppSelector(state => state.league.leagues);
+		const leaguesStatus = useAppSelector(state => state.league.status);
+		const error = useAppSelector(state => state.league.error);
 
-	const [dropown, setDropown] = useState<boolean>(false);
+		const [dropown, setDropown] = useState<boolean>(false);
 
-	useEffect(() => {
-		if (leaguesStatus === "idle") {
-			dispatch(fetchLeagues());
-		}
-	}, [dispatch, leaguesStatus]);
+		useEffect(() => {
+			if (leaguesStatus === "idle") {
+				dispatch(fetchLeagues());
+			}
+		}, [dispatch, leaguesStatus]);
 
-	const toggleDropdown = (): void => {
-		setDropown(prev => !prev);
-	};
+		const toggleDropdown = (): void => {
+			setDropown(prev => !prev);
+		};
 
-	if (leaguesStatus === "loading") return <Loader />;
+		if (leaguesStatus === "loading") return <Loader />;
 
-	if (leaguesStatus === "failed" || leagues[0] === undefined) return <Error />;
+		if (leaguesStatus === "failed" || leagues[0] === undefined) return <Error />;
 
-	if (leaguesStatus === "succeeded")
-		return (
-			<Container>
-				<header>
-					<nav className={styles.nav}>
-						<div className={styles.left}>
-							<div className={styles.pulse}></div>
-							<Link to="/livescores">Live Scores</Link>
-						</div>
-						<div className={styles.center}>
-							<img src={logo} alt="logo" />
-						</div>
-						<div className={styles.right}>
-							<div className={styles.navItem}>
-								<Link to="/">Home</Link>
+		if (leaguesStatus === "succeeded")
+			return (
+				<Container>
+					<header>
+						<nav className={styles.nav}>
+							<div className={styles.left}>
+								<div className={styles.pulse}></div>
+								<Link to="/livescores">Live Scores</Link>
 							</div>
-							<div className={styles.navItem} onClick={toggleDropdown} key="leagues">
-								<p>Leagues</p>
-								{dropown ? (
-									<FontAwesomeIcon icon={faCaretUp} />
-								) : (
-									<FontAwesomeIcon icon={faCaretDown} />
-								)}
-								<div
-									className={
-										dropown
-											? `${styles.dropdown} ${styles.active}`
-											: styles.dropdown
-									}
-								>
-									{leagues.map((league, i) => (
-										<div key={i} className={styles.dropdownItem}>
-											<div className={styles.img}>
-												<img
-													className={styles.leagueIcon}
-													src={league.league.logo}
-													alt=""
-												/>
-											</div>
-											<div>
-												<Link
-													to={`/leagues/${slugify(
-														league.league.name
-													)}/standing`}
-												>
-													{league.league.name}
-												</Link>
-											</div>
-										</div>
-									))}
+							<div className={styles.center}>
+								<img src={logo} alt="logo" />
+							</div>
+							<div className={styles.right}>
+								<div className={styles.navItem}>
+									<Link to="/">Home</Link>
 								</div>
+								<div
+									className={styles.navItem}
+									onClick={toggleDropdown}
+									key="leagues"
+								>
+									<p>Leagues</p>
+									{dropown ? (
+										<FontAwesomeIcon icon={faCaretUp} />
+									) : (
+										<FontAwesomeIcon icon={faCaretDown} />
+									)}
+									<div
+										className={
+											dropown
+												? `${styles.dropdown} ${styles.active}`
+												: styles.dropdown
+										}
+									>
+										{leagues.map((league, i) => (
+											<div key={i} className={styles.dropdownItem}>
+												<div className={styles.img}>
+													<img
+														className={styles.leagueIcon}
+														src={league.league.logo}
+														alt=""
+													/>
+												</div>
+												<div>
+													<Link
+														to={`/leagues/${slugify(
+															league.league.name
+														)}/standing`}
+													>
+														{league.league.name}
+													</Link>
+												</div>
+											</div>
+										))}
+									</div>
+								</div>
+
+								{/* <div className={styles.navItem}>Transfers</div> */}
 							</div>
+						</nav>
+					</header>
+				</Container>
+			);
 
-							{/* <div className={styles.navItem}>Transfers</div> */}
-						</div>
-					</nav>
-				</header>
-			</Container>
-		);
-
-	return <Error text={error} />;
-});
+		return <Error text={error} />;
+	}
+);
