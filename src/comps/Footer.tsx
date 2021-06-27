535adaedbb70d698195959f83adcc5f392c446e1
FarDean <64462409+FarDean@users.noreply.github.com>
2021-06-27 19:52:29 +0430

    some style to foooter

--- src/comps/Footer.tsx
+++ src/comps/Footer.tsx
@@ -5,6 +5,7 @@ export const Footer: React.FC = (): JSX.Element => {
 		<footer className={styles.footer} translate="no">
 			Copyright @2021 | Designed by{" "}
 			<span>
+				{" "}
 				<a href="https://github.com/FarDean" rel="noreferrer" target="_blank">
 					FarDean
 				</a>
