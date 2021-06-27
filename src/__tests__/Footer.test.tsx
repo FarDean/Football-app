1a805030af874f39fd827a179770214abd41c1d6
FarDean <64462409+FarDean@users.noreply.github.com>
2021-06-27 12:09:20 +0430

    created test folder

---
+++ src/__tests__/Footer.test.tsx
@@ -0,0 +1,7 @@
+import { screen, render } from "@testing-library/react";
+import { Footer } from "../comps/Footer";
+
+test("should show the footer", () => {
+	render(<Footer />);
+	expect(screen.getByText("footer")).toBeInTheDocument();
+});
--- src/comps/Footer.tsx
+++ src/comps/Footer.tsx
@@ -1,3 +1,3 @@
 export const Footer: React.FC = (): JSX.Element => {
-	return <div>footer</div>;
+	return <div translate="no">footer</div>;
 };
