01d8c4bf908d3ef7a616f3f63a4b4d53772c54be
FarDean <64462409+FarDean@users.noreply.github.com>
2021-06-27 18:30:05 +0430

    added react-tabs to teamDeatail compononte

--- src/comps/TeamDetail.tsx
+++ src/comps/TeamDetail.tsx
@@ -8,6 +8,8 @@ import { Error } from "./utils/Error";
 import { Hero } from "./utils/Hero";
 import { Container } from "./utils/Container";
 import styles from "./../styles/TeamDetail.module.css";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
 
 export const TeamDetail: React.FC = (): JSX.Element => {
 	const { leagueName, teamId } = useParams<{ leagueName: string; teamId: string }>();
@@ -38,101 +40,108 @@ export const TeamDetail: React.FC = (): JSX.Element => {
 			<>
 				<Hero icon={team.team.logo} text={team.team.name} />
 				<Container>
-					<table className={styles.table}>
-						<tbody>
-							<tr>
-								<th></th>
-								<th>Home</th>
-								<th>Away</th>
-								<th>Total</th>
-							</tr>
-							<tr>
-								<th>Average Goals Scored</th>
-								<td>{team.goals.for.average.home}</td>
-								<td>{team.goals.for.average.away}</td>
-								<td>{team.goals.for.average.total}</td>
-							</tr>
-							<tr>
-								<th>Total Goals Scored</th>
-								<td>{team.goals.for.total.home}</td>
-								<td>{team.goals.for.total.away}</td>
-								<td>{team.goals.for.total.total}</td>
-							</tr>
-							<tr>
-								<th>Average Goals Conceded</th>
-								<td>{team.goals.against.average.home}</td>
-								<td>{team.goals.against.average.away}</td>
-								<td>{team.goals.against.average.total}</td>
-							</tr>
-							<tr>
-								<th>Total Goals Conceded</th>
-								<td>{team.goals.against.total.home}</td>
-								<td>{team.goals.against.total.away}</td>
-								<td>{team.goals.against.total.total}</td>
-							</tr>
-							<tr>
-								<th>Most Goals Scored</th>
-								<td>{team.biggest!.goals!.for.home}</td>
-								<td>{team.biggest!.goals!.for.away}</td>
-							</tr>
-							<tr>
-								<th>Most Goals Conceded</th>
-								<td>{team.biggest!.goals!.against.home}</td>
-								<td>{team.biggest!.goals!.against.away}</td>
-							</tr>
-							<tr>
-								<th>Biggest Win</th>
-								<td>{team.biggest!.wins!.home}</td>
-								<td>{team.biggest!.wins!.away}</td>
-							</tr>
-							<tr>
-								<th>Biggest Lose</th>
-								<td>{team.biggest!.loses!.home}</td>
-								<td>{team.biggest!.loses!.away}</td>
-							</tr>
-							<tr>
-								<th>Clean Sheets</th>
-								<td>{team.clean_sheet.home}</td>
-								<td>{team.clean_sheet.away}</td>
-								<td>{team.clean_sheet.total}</td>
-							</tr>
-							<tr>
-								<th>Matches Played</th>
-								<td>{team.fixtures.played.home}</td>
-								<td>{team.fixtures.played.away}</td>
-								<td>{team.fixtures.played.total}</td>
-							</tr>
-							<tr>
-								<th>Wins</th>
-								<td>{team.fixtures.wins.home}</td>
-								<td>{team.fixtures.wins.away}</td>
-								<td>{team.fixtures.wins.total}</td>
-							</tr>
-							<tr>
-								<th>Draws</th>
-								<td>{team.fixtures.draws.home}</td>
-								<td>{team.fixtures.draws.away}</td>
-								<td>{team.fixtures.draws.total}</td>
-							</tr>
-							<tr>
-								<th>Loses</th>
-								<td>{team.fixtures.loses.home}</td>
-								<td>{team.fixtures.loses.away}</td>
-								<td>{team.fixtures.loses.total}</td>
-							</tr>
-							<tr>
-								<th></th>
-								<th>Win</th>
-								<th>Draw</th>
-								<th>Lose</th>
-							</tr>
-							<tr>
-								<th>Streak</th>
-								<td>{team.biggest!.streak!.wins}</td>
-								<td>{team.biggest!.streak!.draws}</td>
-								<td>{team.biggest!.streak!.loses}</td>
-							</tr>
-							{/* <tr>
+					<Tabs>
+						<TabList>
+							<Tab>Statistics</Tab>
+							<Tab>Fixtures</Tab>
+						</TabList>
+
+						<TabPanel>
+							<table className={styles.table}>
+								<tbody>
+									<tr>
+										<th></th>
+										<th>Home</th>
+										<th>Away</th>
+										<th>Total</th>
+									</tr>
+									<tr>
+										<th>Average Goals Scored</th>
+										<td>{team.goals.for.average.home}</td>
+										<td>{team.goals.for.average.away}</td>
+										<td>{team.goals.for.average.total}</td>
+									</tr>
+									<tr>
+										<th>Total Goals Scored</th>
+										<td>{team.goals.for.total.home}</td>
+										<td>{team.goals.for.total.away}</td>
+										<td>{team.goals.for.total.total}</td>
+									</tr>
+									<tr>
+										<th>Average Goals Conceded</th>
+										<td>{team.goals.against.average.home}</td>
+										<td>{team.goals.against.average.away}</td>
+										<td>{team.goals.against.average.total}</td>
+									</tr>
+									<tr>
+										<th>Total Goals Conceded</th>
+										<td>{team.goals.against.total.home}</td>
+										<td>{team.goals.against.total.away}</td>
+										<td>{team.goals.against.total.total}</td>
+									</tr>
+									<tr>
+										<th>Most Goals Scored</th>
+										<td>{team.biggest!.goals!.for.home}</td>
+										<td>{team.biggest!.goals!.for.away}</td>
+									</tr>
+									<tr>
+										<th>Most Goals Conceded</th>
+										<td>{team.biggest!.goals!.against.home}</td>
+										<td>{team.biggest!.goals!.against.away}</td>
+									</tr>
+									<tr>
+										<th>Biggest Win</th>
+										<td>{team.biggest!.wins!.home}</td>
+										<td>{team.biggest!.wins!.away}</td>
+									</tr>
+									<tr>
+										<th>Biggest Lose</th>
+										<td>{team.biggest!.loses!.home}</td>
+										<td>{team.biggest!.loses!.away}</td>
+									</tr>
+									<tr>
+										<th>Clean Sheets</th>
+										<td>{team.clean_sheet.home}</td>
+										<td>{team.clean_sheet.away}</td>
+										<td>{team.clean_sheet.total}</td>
+									</tr>
+									<tr>
+										<th>Matches Played</th>
+										<td>{team.fixtures.played.home}</td>
+										<td>{team.fixtures.played.away}</td>
+										<td>{team.fixtures.played.total}</td>
+									</tr>
+									<tr>
+										<th>Wins</th>
+										<td>{team.fixtures.wins.home}</td>
+										<td>{team.fixtures.wins.away}</td>
+										<td>{team.fixtures.wins.total}</td>
+									</tr>
+									<tr>
+										<th>Draws</th>
+										<td>{team.fixtures.draws.home}</td>
+										<td>{team.fixtures.draws.away}</td>
+										<td>{team.fixtures.draws.total}</td>
+									</tr>
+									<tr>
+										<th>Loses</th>
+										<td>{team.fixtures.loses.home}</td>
+										<td>{team.fixtures.loses.away}</td>
+										<td>{team.fixtures.loses.total}</td>
+									</tr>
+									<tr>
+										<th></th>
+										<th>Win</th>
+										<th>Draw</th>
+										<th>Lose</th>
+									</tr>
+									<tr>
+										<th>Streak</th>
+										<td>{team.biggest!.streak!.wins}</td>
+										<td>{team.biggest!.streak!.draws}</td>
+										<td>{team.biggest!.streak!.loses}</td>
+									</tr>
+									{/* <tr>
 								<th></th>
 								{Object.entries(team.cards.yellow).map(([key, value]) => (
 									<th key={key}>{key}`</th>
@@ -161,18 +170,23 @@ export const TeamDetail: React.FC = (): JSX.Element => {
 									</td>
 								))}
 							</tr> */}
-							<tr>
-								<th></th>
-								<th>Total</th>
-								<th>Scored</th>
-							</tr>
-							<tr>
-								<th>Penalties</th>
-								<td>{team.penalty.total}</td>
-								<td>{team.penalty.scored.total}</td>
-							</tr>
-						</tbody>
-					</table>
+									<tr>
+										<th></th>
+										<th>Total</th>
+										<th>Scored</th>
+									</tr>
+									<tr>
+										<th>Penalties</th>
+										<td>{team.penalty.total}</td>
+										<td>{team.penalty.scored.total}</td>
+									</tr>
+								</tbody>
+							</table>
+						</TabPanel>
+						<TabPanel>
+							<h2>Fixtures</h2>
+						</TabPanel>
+					</Tabs>
 				</Container>
 			</>
 		);
